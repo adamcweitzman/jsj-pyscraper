9d556033b3e6cef8935accc78b01b7f9aebb689c
Adam Cole Weitzman <adamcweitzman@gmail.com>
2016-09-01 18:34:49 -0700

    set up scrappy framework and punch in beautiful soup to spider.py

---
+++ indeed_scraper/indeed_scraper/items.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class IndeedScraperItem(scrapy.Item):
+    title = scrappy.Filed()
+    description = scrappy.Filed()
+    blurb = scrappy.Filed()
+    location = scrappy.Filed()
+    pass
